2c8e4c4865556bafba852ab983a6580b23853f9c
Katie Masiello <ka.masiello@gmail.com>
2021-01-02 22:50:18 -0800

    create demo API

---
+++ R/coin_toss_api/plumber.R
@@ -0,0 +1,19 @@
+library(plumber)
+
+#* @apiTitle Coin Toss API
+
+#* Toss a coin once and give the result
+#* @get /coin_toss
+
+function() {
+  sample(c("heads", "tails"), 1, prob = c(0.5, 0.5), replace = TRUE)
+}
+
+#* Toss a multiple times and summarize output
+#* @param n the number of times to toss the coin
+#* @get /multi_toss
+#* @serializer rds
+
+function(n) {
+  table(sample(c("heads", "tails"), n, prob = c(0.5, 0.5), replace = TRUE))
+}
